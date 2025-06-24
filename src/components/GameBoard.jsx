fad1f886e247a56c46907da158c34ec2d3d401d6
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-24 20:06:13 +0300

    feat(component): enhance Player and GameBoard components with active player functionality

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,39 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ changePlayer, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // * Updating object state in an immutable way
+  // * As per React's best practices, we should not mutate the state directly.
+  // * Instead, we create a new copy of the state and update that copy.
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameBoard((previousBoard) => {
+      const updatedBoard = [
+        ...previousBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    changePlayer();
+  };
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
-            {row.map((cell, cellIndex) => (
-              <li key={cellIndex} className="game-cell">
-                <button>{cell}</button>
+            {row.map((col, colIndex) => (
+              <li key={colIndex} className="game-cell">
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {col}
+                </button>
               </li>
             ))}
           </ol>
