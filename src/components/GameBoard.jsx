d38f5247f64ad070efa2083dec2ab0835d603763
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-26 01:41:43 +0300

    refactor(component): simplify GameBoard component by removing unused state and logic

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,50 +1,17 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ changePlayer, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // * Updating object state in an immutable way
-  // * As per React's best practices, we should not mutate the state directly.
-  // * Instead, we create a new copy of the state and update that copy.
-  /* 
-    const handleSelectSquare = (rowIndex, colIndex) => {
-      setGameBoard((previousBoard) => {
-        const updatedBoard = [
-          ...previousBoard.map((innerArray) => [...innerArray]),
-        ];
-        updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-        return updatedBoard;
-      });
-
-      changePlayer();
-    };
-  */
-
+export default function GameBoard({ changePlayer, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
-            {row.map((col, colIndex) => (
+            {row.map((playerSymbol, colIndex) => (
               <li key={colIndex} className="game-cell">
                 <button
                   onClick={() => changePlayer(rowIndex, colIndex)}
-                  disabled={col !== null}
+                  disabled={playerSymbol !== null}
+                  className={playerSymbol ? `symbol-${playerSymbol}` : ""}
                 >
-                  {col}
+                  {playerSymbol}
                 </button>
               </li>
             ))}
